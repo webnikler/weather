8ff265a9bce9a849f528a1f87f04b85754636c98
webnikler <webnikler@gmail.com>
2022-08-21 04:56:58 +0300

    add geo location hook

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useLocation } from './core/location/location';
 import HomeScreen from './screens/HomeScreen';
 import TommorowScreen from './screens/TommorowScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = (): JSX.Element => {
+  const { loading, coords, error } = useLocation();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
